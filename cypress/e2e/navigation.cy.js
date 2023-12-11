3f87cc01f3fe9a39293210edaf8277de7d2b31a5
Ernesto Jr Fundal Osorio <114483893+nyolarraklay@users.noreply.github.com>
2023-12-11 14:50:01 +0100

    create e2e testing

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -46,4 +46,166 @@ describe("Auction Website: Unauthorized user", () => {
     cy.get("#search > .form-control").type("zzz").type("{enter}");
     cy.url().should("include", "src/html/listings.html");
   });
+
+  it("should not submit the log in form", () => {
+    cy.visit("/");
+
+    cy.get("#email")
+      .type("fake@stud.noroff.no", { force: true, delay: 100 })
+      .should("have.value", "fake@stud.noroff.no");
+    cy.get("#password").type("fakepassword", { force: true, delay: 100 });
+
+    cy.get("#loginForm").trigger("submit");
+    cy.on("window:alert", (t) => {
+      expect(t).to.contains({
+        errors: [{ message: "Invalid email or password" }],
+        status: "Unauthorized",
+        statusCode: 401,
+      });
+    });
+  });
+});
+
+describe("Auction Website: Authorized user", () => {
+  it("should submit the log in form and access profile page", () => {
+    cy.visit("/src/html/register.html");
+    cy.get(".navbar-nav > :nth-child(3)").click();
+    cy.url().should("include", "/");
+
+    cy.get("#email").type("asasa@noroff.no");
+    cy.get("#password").type("password");
+
+    cy.get("#loginForm").submit();
+
+    cy.url().should("include", "/src/html/homePage.html", {
+      timeout: 10000,
+    });
+    cy.get(".dropdown > .btn").click();
+    cy.get(".dropdown-menu > :nth-child(1) > .fs-7").click();
+    cy.url().should("include", "src/html/profilePage.html");
+  });
+
+  it("should update the user's avatar", () => {
+    cy.visit("/src/html/register.html");
+    cy.get(".navbar-nav > :nth-child(3)").click();
+    cy.url().should("include", "/");
+
+    cy.get("#email").type("asasa@noroff.no");
+    cy.get("#password").type("password");
+
+    cy.get("#loginForm").submit();
+
+    cy.url().should("include", "/src/html/homePage.html", {
+      timeout: 10000,
+    });
+    cy.get(".dropdown > .btn").click();
+    cy.get(".dropdown-menu > :nth-child(1) > .fs-7").click();
+    cy.url().should("include", "src/html/profilePage.html");
+    cy.get(".profileDetails > .editButton").click();
+    cy.get("#avatar").type("https://source.unsplash.com/random/60");
+    cy.get("#updateRegisterForm").submit();
+    cy.url().should("include", "src/html/profilePage.html");
+  });
+
+  it("should create a new Listing", () => {
+    cy.visit("/src/html/register.html");
+    cy.get(".navbar-nav > :nth-child(3)").click();
+    cy.url().should("include", "/");
+
+    cy.get("#email").type("asasa@noroff.no");
+    cy.get("#password").type("password");
+
+    cy.get("#loginForm").submit();
+
+    cy.url().should("include", "/src/html/homePage.html", {
+      timeout: 10000,
+    });
+    cy.get(".dropdown > .btn").click();
+    cy.get(".dropdown-menu > :nth-child(2) > .fs-7").click();
+    cy.url().should("include", "src/html/profileListings.html");
+    cy.get('[data-bs-toggle="modal"]').click();
+    cy.wait(1000);
+    cy.get("#postTitle").type("My sample Listing");
+    cy.get("#postDescription").type("Description of my sample Listing");
+    cy.get("#postTags").type("sample");
+    cy.get("#postMedia").type("https://source.unsplash.com/random/63");
+    cy.get("#postEnds").click();
+    cy.get("#newPost").submit();
+    cy.url().should("include", "src/html/profileListings.html");
+  });
+
+  it("should may view bids on a Listing", () => {
+    cy.visit("/src/html/register.html");
+    cy.get(".navbar-nav > :nth-child(3)").click();
+    cy.url().should("include", "/");
+
+    cy.get("#email").type("asasa@noroff.no");
+    cy.get("#password").type("password");
+
+    cy.get("#loginForm").submit();
+
+    cy.url().should("include", "/src/html/homePage.html", {
+      timeout: 10000,
+    });
+    cy.get(".dropdown > .btn").click();
+    cy.get(".dropdown-menu > :nth-child(2) > .fs-7").click();
+    cy.url().should("include", "src/html/profileListings.html");
+    cy.get(
+      '[href="/src/html/details.html?id=e86df5fb-9984-4475-ba30-2a14fdd043e7"] > .card-body > .card-img-bottom'
+    ).click();
+    cy.url().should("include", "src/html/details.html");
+  });
+
+  it("should may add a bid to another user's listing", () => {
+    cy.visit("/src/html/register.html");
+    cy.get(".navbar-nav > :nth-child(3)").click();
+    cy.url().should("include", "/");
+
+    cy.get("#email").type("asasa@noroff.no");
+    cy.get("#password").type("password");
+
+    cy.get("#loginForm").submit();
+
+    cy.url().should("include", "/src/html/homePage.html", {
+      timeout: 10000,
+    });
+    cy.get(".dropdown > .btn").click();
+    cy.get(".dropdown-menu > :nth-child(2) > .fs-7").click();
+    cy.url().should("include", "src/html/profileListings.html");
+    cy.get(
+      '[href="/src/html/details.html?id=e86df5fb-9984-4475-ba30-2a14fdd043e7"] > .card-body > .card-img-bottom'
+    ).click();
+    cy.url().should("include", "src/html/details.html");
+    cy.get(".mt-3 > :nth-child(1) > .btn").click();
+    cy.get("#amount").type("10");
+    cy.get("#bidForm").submit();
+  });
+
+  it("should successfully logout the website", () => {
+    cy.visit("/src/html/register.html");
+    cy.get(".navbar-nav > :nth-child(3)").click();
+    cy.url().should("include", "/");
+
+    cy.get("#email").type("asasa@noroff.no");
+    cy.get("#password").type("password");
+
+    cy.get("#loginForm").submit();
+
+    cy.url().should("include", "/src/html/homePage.html", {
+      timeout: 10000,
+    });
+    cy.get(".dropdown > .btn").click();
+    cy.get(".dropdown-menu > :nth-child(1) > .fs-7").click();
+    cy.url().should("include", "src/html/profilePage.html");
+    cy.get("#logButton").click();
+    cy.url().should("include", "/index.html");
+  });
+
+  it("should successfully clear the Local Storage", () => {
+    cy.visit("index.html", {
+      onBeforeLoad(win) {
+        win.localStorage.clear();
+      },
+    });
+  });
 });
